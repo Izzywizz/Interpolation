f941cc73765813f48c895a3eb7b96aa18dddb287
Isfand <izzywhizzzz@gmail.com>
2019-03-28 10:02:40 +0000

    using zeno paradox to interpolate a sphere following movement

---
+++ Interpolation/Assets/ZenosFollower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZenosFollower : MonoBehaviour
+{
+    public GameObject poi; // Point of Interest
+    public float u = 0.1f;
+    public Vector3 p0, p1, p01;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        //Get the postion of this and the poi
+        p0 = this.transform.position;
+        p1 = poi.transform.position;
+
+        // Interpolate between the two, using standard formula
+        p01 = (1 - u) * p0 + u * p1;
+
+        // move this to the new position
+        this.transform.position = p01;
+    }
+}
